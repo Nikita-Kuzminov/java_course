855c9c4b62e217428a62dfcc18dae16d332938fd
Nikita-Kuzminov <nkuzminov220299@gmail.com>
2021-11-11 12:24:56 +0300

    Changed name of method

--- LocalMinimum/src/LocalMinimum.java
+++ LocalMinimum/src/LocalMinimum.java
@@ -2,10 +2,10 @@ import java.lang.reflect.Array;
 
 public class LocalMinimum {
     public static void main(String[] args) {
-        enterArray();
+        findAndCountLocalMin();
     }
 
-    static void enterArray() {
+    static void findAndCountLocalMin() {
         int localMinimumCount = 0;
         int[] array = {22, 50, 34, 109, 473, 245, 378};
         for (int i = 1; i < array.length; i++) {
